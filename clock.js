0ccf16a2dc5082d32a8a6b33bda8ee6fbb6eaa35
Koichi Okutsu <okutsu@pej.jp>
2021-02-18 22:12:46 +0900

    [change] Access every 10 minutes to avoid sleeping.

--- clock.js
+++ clock.js
@@ -1,9 +1,31 @@
-var CronJob = require('cron').CronJob;
-new CronJob({
-	cronTime: "0 0 * * * *",//every hour
+const CronJob = require('cron').CronJob;
+const request = require('request');
+
+const get_rss = function (url) {
+	return new Promise((resolve, reject) => {
+		request(url + '?' + (new Date()).getTime(), (error, response, body) => {
+			// エラーチェック
+			if (error !== null) {
+				reject(error);
+			}
+			// レスポンスコードとHTMLを表示
+			console.log('statusCode:', response && response.statusCode);
+			resolve(true);
+		});
+	})
+};
+
+const job = new CronJob({
+	cronTime: "0 */10 * * * *",//every hour
 	onTick: () => {
 		console.log(new Date());
+		get_rss('https://hatena-rss.herokuapp.com/');
+	},
+	onComplete: (s) => {
+		console.log('onComplete',s);
 	},
 	start: true,
 	timeZone: "Asia/Tokyo"
 });
+
+console.log(job);
